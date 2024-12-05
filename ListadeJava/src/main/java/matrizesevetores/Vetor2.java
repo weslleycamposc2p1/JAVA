1ebf0501c2d3aaf9bdc19103c3989993c346048a
weslleycamposc2p1 <weslleycampos951@gmail.com>
2024-12-05 13:09:09 -0300

    vetor e matrix criado

--- ListadeJava/src/main/java/matrizesevetores/Vetor2.java
+++ ListadeJava/src/main/java/matrizesevetores/Vetor2.java
@@ -1,23 +1,26 @@
 package matrizesevetores;
-import java.util.Scanner;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Vetor1 {
+public class Vetor2 {
 
     public static void main(String[] args) {
 
-        Scanner numeros = new Scanner(System.in);
+
+        Scanner soma = new Scanner(System.in);
 
 
         List<Integer> lista = new ArrayList<Integer>();
-        for (int n = 0; n <5; n++) {
+        for (int n = 0; n <10; n++) {
             System.out.println("Digite um numero: ");
-            lista.add(numeros.nextInt());
+            lista.add(soma.nextInt());
         }
 
+        
+        for (int)
         System.out.println(lista);
-
 
 
     }
